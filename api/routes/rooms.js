cdb3a5ad34544e6a6346a5b982382cf6d0f3ac95
danzaky <danielsaenz-96@hotmail.com>
2023-05-30 23:49:09 -0500

    creación de endpoints y manejo de errores

---
+++ api/routes/rooms.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/:hotelid", createRoom);
+
+//UPDATE
+router.put("/:id", updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GET ALL
+router.get("/", getRooms);
+
+export default router;
